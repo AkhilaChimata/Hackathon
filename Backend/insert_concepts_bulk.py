f8c97dac9daaa14177c6709ce47f500e13f8eabb
Ruchitha Gande <ganderuchitha@gmail.com>
2025-06-15 23:11:32 -0500

    updates

--- Backend/insert_concepts_bulk.py
+++ Backend/insert_concepts_bulk.py
@@ -1,11 +1,12 @@
+import os
 import json
 from pymongo import MongoClient
 import certifi
 from urllib.parse import quote_plus
 
-# MongoDB credentials
-username = quote_plus("AkhilaChimata")
-password = quote_plus("Akhila@9")
+# MongoDB credentials from environment variables
+username = quote_plus(os.environ.get("MONGO_USER", ""))
+password = quote_plus(os.environ.get("MONGO_PASS", ""))
 uri = f"mongodb+srv://{username}:{password}@cluster0.t6p9qr9.mongodb.net/"
 
 # Connect to MongoDB
